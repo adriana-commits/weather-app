3b5193eca194f5ee2a06d21866e6e8478f3a4059
adriana-commits <77590757+adriana-commits@users.noreply.github.com>
2021-03-07 17:01:07 +0100

    Updated form and left column
    added humidity, wind speed, Fahreheit temperature.
    added default search for "Munich" at page load.

--- src/index.js
+++ src/index.js
@@ -16,14 +16,28 @@ let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `${weekday} ${now.getHours()}:${now.getMinutes()}`;
 
 function displayWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
+  let celsius = Math.round(response.data.main.temp);
+  let fahrenheit = Math.round((celsius * 9) / 5 + 32);
   let currentTemperature = document.querySelector("#current-temperature");
-  currentTemperature.innerHTML = `${temperature}°C`;
+  currentTemperature.innerHTML = `${celsius}°C | ${fahrenheit}° F`;
+}
+
+function displayHumidity(response) {
+  let humidity = Math.round(response.data.main.humidity);
+  let currentHumidity = document.querySelector("#current-humidity");
+  currentHumidity.innerHTML = `Humidity: ${humidity}%`;
+}
+
+function displayWindspeed(response) {
+  let windspeed = Math.round(response.data.wind.speed);
+  let currentWindspeed = document.querySelector("#current-windspeed");
+  currentWindspeed.innerHTML = `Windspeed: ${windspeed} km/h`;
 }
 
 function searchCity(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-form").value;
+  let searchInput = document.querySelector("#search-form");
+  let city = searchInput.value;
   let apiKey = "59291e55603857fc768aa2b6fad03a18";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -31,12 +45,14 @@ function searchCity(event) {
     let searchedCity = document.querySelector("#searched-city");
     searchedCity.innerHTML = city;
     displayWeather(response);
+    displayHumidity(response);
+    displayWindspeed(response);
+    searchInput.value = "";
   });
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchCity);
 
-//testing this out
-
-//pana aici testam
+let btn = document.querySelector("#submit-btn");
+btn.click();
